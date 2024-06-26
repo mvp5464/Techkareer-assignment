29f44e24c535639c5d25d1cc305c508f4104780b
mvking5464 <mvking5464@gmail.com>
2024-06-26 11:33:19 +0530

    jobInfo Section added

--- src/components/JobInfo.tsx
+++ src/components/JobInfo.tsx
@@ -2,25 +2,32 @@ import JobInfoComp from "./JobInfoComp";
 
 const JobInfo = () => {
   return (
-    <div>
-      <div className=" flex gap-2">
-        <div>
-          <div>Skills Required</div>
-          <JobInfoComp image={"/figmaLogo.png"} name={"Figma"} />
-          <JobInfoComp image={"/adobeAILogo.png"} name={"Adobe Illustrator"} />
-          <JobInfoComp image={"/adobeXDLogo.png"} name={"Adobe XD"} />
-        </div>
-        <div>
-          <div>Preferred Language</div>
-          <div>English</div>
-        </div>
-        <div>
-          <div>Type</div>
-          <div>Full time</div>
-        </div>
-        <div>
-          <div>Years of Experience</div>
-          <div>3+ Years of Experience</div>
+    <div className="border-b border-[#E7E7E7]">
+      <div className="ml-20 my-5 ">
+        <div className=" flex gap-9">
+          <div className=" space-y-2">
+            <div className=" text-[#6E6D6D]">Skills Required</div>
+            <JobInfoComp image={"/figmaLogo.png"} name={"Figma"} />
+            <JobInfoComp
+              image={"/adobeAILogo.png"}
+              name={"Adobe Illustrator"}
+            />
+            <JobInfoComp image={"/adobeXDLogo.png"} name={"Adobe XD"} />
+          </div>
+          <div>
+            <div className=" text-[#6E6D6D]">Preferred Language</div>
+            <div className=" text-[#3D3D3D] font-bold">English</div>
+          </div>
+          <div>
+            <div className=" text-[#6E6D6D]">Type</div>
+            <div className=" text-[#3D3D3D] font-bold">Full time</div>
+          </div>
+          <div className=" ml-20 ">
+            <div className=" text-[#6E6D6D]">Years of Experience</div>
+            <div className=" text-[#3D3D3D] font-bold">
+              3+ Years of Experience
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/JobInfoComp.tsx
+++ src/components/JobInfoComp.tsx
@@ -4,15 +4,17 @@ const JobInfoComp = ({ image, name }: { image: string; name: string }) => {
   return (
     <>
       <div className=" flex">
-        <Image
-          src={image}
-          alt="Figma Logo"
-          className="dark:invert rounded-full"
-          width={30}
-          height={0}
-          priority
-        />
-        <span>{name}</span>
+        <div className=" border flex gap-2 px-1 py-1 rounded-lg text-xs">
+          <Image
+            src={image}
+            alt={name}
+            className="shrink-0"
+            width={20}
+            height={0}
+            priority
+          />
+          <span className=" text-[#344054]">{name}</span>
+        </div>
       </div>
     </>
   );
